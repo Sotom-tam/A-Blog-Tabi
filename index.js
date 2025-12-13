88a513004747faa27e7fa8a81424db1d8b779ced
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2025-12-13 22:03:18 +0100

    So I started working on the styling and I perfected the function of the site.
    So I created the structure of the home page,
    Also styled the home page
    Functionality works but I need to make it better.
    It has  a few bugs currently.
    I also need to perfect the styling of the  website

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-import express from "express"
-import bodyParser from "body-parser"
-const app=express()
-const port=3000;
-app.use(express.urlencoded({extended:"true"}))
-app.use(express.static("public"))
+import express from "express";
+import bodyParser from "body-parser";
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs")
-})
+const app = express();
+const port = 3000;
+let month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let x = new Date();
 
+x.getDate();
+let date = month[x.getMonth()] + " " + x.getDate() + " " + x.getFullYear();
 
-app.get("/createNewEntry",(req,res)=>{
-    res.render("newEntry.ejs")
-})
+function createNewEntry() {}
+app.use(express.urlencoded({ extended: "true" }));
+app.use(express.static("public"));
+app.use("/bootstrap", express.static("node_modules/bootstrap/dist"));
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.listen(port,()=>{
-    console.log("Listening at Port 3000")
-})
+app.get("/createNewEntry", (req, res) => {
+  res.render("newEntry.ejs");
+});
+app.post("/creaTeNewEntry", (req, res) => {
+  let newEntry = {
+    date: date,
+    title: req.body["title"],
+    content: req.body["content"],
+    tag: req.body["tag"],
+  };
+  res.render("index.ejs", newEntry);
+  console.log(newEntry);
+});
+
+app.listen(port, () => {
+  console.log("Listening at Port 3000");
+});
