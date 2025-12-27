8712a415ff13c5714f35dbc3e25dbc7baf652950
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2025-12-27 18:22:06 +0100

    This commit contains Changes made to perfect the create A new entry function
    I made an array of posts, that contains all the posts. Each Post is an object that has properties like the date, title, content , tag.
    I was able to pass this array to the ejs file by enclosing it in array.
    
    I used a forEACH loop to render all the pre-made posts in the array on the home page.
    When a new post is created it's unshfited to the beginning of the array and hence is shown as the first post. With it's date saved.
    
    The same index.js page is rendered as a response to the POST /createNewEntry.
    
    For the Post Content, I changed it from an input to a textarea element, so we can see multiple lines as the user types.
    
    I also created a  new ejs file called reading view, this is to show the full post when it's clicked.

--- index.js
+++ index.js
@@ -18,16 +18,69 @@ let month = [
   "December",
 ];
 let x = new Date();
-x.getDate();
 let date = month[x.getMonth()] + " " + x.getDate() + " " + x.getFullYear();
-let blogPost = [];
-
+let y = new Date("2025-11-23");
+let tag = "God,Growing,Process,Faith".split(",");
+let blogPost = [
+  {
+    // id:
+    //   "post-" +
+    //   Number(new Date("2025-12-11").getMonth() + 1) +
+    //   "-" +
+    //   +new Date("2025-12-11").getDate() +
+    //   "-" +
+    //   +new Date("2025-12-11").getFullYear(),
+    date:
+      month[new Date("2025-12-11").getMonth()] +
+      " " +
+      new Date("2025-12-11").getDate() +
+      " " +
+      new Date("2025-12-11").getFullYear(),
+    title: "Who is God?",
+    content:
+      "I'm sure we've all heard about the existence of an all powerful being. \n About how the world may have been created not by a series of coincidences but Intentionally \n That someone thought about why, what before he put things together and that we're all made for a purpose.\n I think believeing in God is the logical decision.\n I'm not a religious or brainwashed person, but I think only a being who has some sort of reasoning and intelligence could've cooked up this world to be what we see today.\n It is this same logic and reason that maskes me stumble in my faith sometimes. Sometimes I have doubt that God isn't exactly who he claims to be.\n But then again through reasoning, searching, and of course me being an intellignt being I come to find that He is who he says he is. I'm still searching and I don't think I ever will, I sometimes don't have the answers, but My faith is strong ",
+    tag: tag,
+  },
+  {
+    // id:
+    //   "post-" +
+    //   Number(y.getMonth() + 1) +
+    //   "-" +
+    //   +y.getDate() +
+    //   "-" +
+    //   +y.getFullYear(),
+    date: month[y.getMonth()] + " " + y.getDate() + " " + y.getFullYear(),
+    title: "Who is God?",
+    content:
+      "I'm sure we've all heard about the existence of an all powerful being. \n About how the world may have been created not by a series of coincidences but Intentionally \n That someone thought about why, what before he put things together and that we're all made for a purpose.\n I think believeing in God is the logical decision.\n I'm not a religious or brainwashed person, but I think only a being who has some sort of reasoning and intelligence could've cooked up this world to be what we see today.\n It is this same logic and reason that maskes me stumble in my faith sometimes. Sometimes I have doubt that God isn't exactly who he claims to be.\n But then again through reasoning, searching, and of course me being an intellignt being I come to find that He is who he says he is. I'm still searching and I don't think I ever will, I sometimes don't have the answers, but My faith is strong ",
+    tag: tag,
+  },
+  {
+    // id:
+    //   "post-" +
+    //   Number(new Date("2025-12-18").getMonth() + 1) +
+    //   "-" +
+    //   +new Date("2025-12-18").getDate() +
+    //   "-" +
+    //   +new Date("2025-12-18").getFullYear(),
+    date:
+      month[new Date("2025-12-18").getMonth()] +
+      " " +
+      new Date("2025-12-18").getDate() +
+      " " +
+      new Date("2025-12-18").getFullYear(),
+    title: "Who is God?",
+    content:
+      "I'm sure we've all heard about the existence of an all powerful being. \n About how the world may have been created not by a series of coincidences but Intentionally \n That someone thought about why, what before he put things together and that we're all made for a purpose.\n I think believeing in God is the logical decision.\n I'm not a religious or brainwashed person, but I think only a being who has some sort of reasoning and intelligence could've cooked up this world to be what we see today.\n It is this same logic and reason that maskes me stumble in my faith sometimes. Sometimes I have doubt that God isn't exactly who he claims to be.\n But then again through reasoning, searching, and of course me being an intellignt being I come to find that He is who he says he is. I'm still searching and I don't think I ever will, I sometimes don't have the answers, but My faith is strong ",
+    tag: tag,
+  },
+];
 function createNewEntry(req, res) {
   let newEntry = {
     date: date,
     title: req.body["title"],
     content: req.body["content"],
-    tag: req.body["tag"],
+    tag: req.body["tag"].split(","),
   };
   blogPost.push(newEntry);
   console.log(blogPost);
@@ -37,25 +90,30 @@ app.use(express.urlencoded({ extended: "true" }));
 app.use(express.static("public"));
 app.use("/bootstrap", express.static("node_modules/bootstrap/dist"));
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    posts: blogPost,
+  });
 });
 
 app.get("/createNewEntry", (req, res) => {
   res.render("newEntry.ejs");
 });
-app.post("/creaTeNewEntry", (req, res) => {
+app.post("/createNewEntry", (req, res) => {
   let newEntry = {
+    // id: `post-${date}`,
     date: date,
     title: req.body["title"],
     content: req.body["content"],
     tag: req.body["tag"],
   };
-  blogPost.push(newEntry);
-  console.log(blogPost);
-  res.render("index.ejs", newEntry);
-  console.log(newEntry);
+  blogPost.unshift(newEntry);
+  res.render("index.ejs", {
+    posts: blogPost,
+  });
 });
 
 app.listen(port, () => {
   console.log("Listening at Port 3000");
 });
+// let str = "LHo,hdl,ho";
+// console.log(str.split(","), tag);
